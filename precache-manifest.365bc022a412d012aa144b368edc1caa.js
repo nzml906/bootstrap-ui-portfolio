ac049e3cfb61bba0718700568de376dd0565b827
Nazmul <grey000906@gmail.com>
2020-07-18 06:04:44 +0600

    deploy portfolio

---
+++ precache-manifest.365bc022a412d012aa144b368edc1caa.js
@@ -0,0 +1,382 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2975aa9d757f961b3743",
+    "url": "/bootstrap-ui-portfolio/css/app.4a54737f.css"
+  },
+  {
+    "revision": "10d0b5b87cda069eb7113d32a9dd9eb2",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/adobe.svg"
+  },
+  {
+    "revision": "683a9f8503e61cead84c3e46ec82ce54",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/amazon.svg"
+  },
+  {
+    "revision": "c983195344a6c98e504d23c753b4d063",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/apple.svg"
+  },
+  {
+    "revision": "7be91ed004681907074b7273a093cfd3",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/cisco.svg"
+  },
+  {
+    "revision": "4be3a664237bfcf333cd702664d0ab7e",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/discovery.svg"
+  },
+  {
+    "revision": "140198db344a328efba8c52b59a87e24",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/ebay.svg"
+  },
+  {
+    "revision": "b679a54e46e302b68155dd1b0d50a6f7",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/ibm.svg"
+  },
+  {
+    "revision": "956b1d4732851d265ecd391e0ddaf536",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/intel.svg"
+  },
+  {
+    "revision": "ad68c662b5916859d1772914481e59c6",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/orange.svg"
+  },
+  {
+    "revision": "f44227fdb5209b8d35d9fb006dc04dc6",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/panasonic.svg"
+  },
+  {
+    "revision": "d1dd2d9806fdd3b5d1f7706dc9b7444e",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/salesforce.svg"
+  },
+  {
+    "revision": "e9b961da822f82264c33d4c4e930a151",
+    "url": "/bootstrap-ui-portfolio/imgs/customers/samsung.svg"
+  },
+  {
+    "revision": "15b92884cbc16135d5cb65c86dad7daf",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/android.svg"
+  },
+  {
+    "revision": "b429ba1807b222bfc535eebf0119cbb3",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/basketball.svg"
+  },
+  {
+    "revision": "fcc2be8a132271c5ac13da6196a1dac0",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/browsers.svg"
+  },
+  {
+    "revision": "daf3c4681ee00dfdbf9c15191573556b",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/celebration.svg"
+  },
+  {
+    "revision": "a7c7ce036bdc53591304fbef440219d6",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/chat.svg"
+  },
+  {
+    "revision": "d6233b4878d9d950871500309cd015f1",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/chatting.svg"
+  },
+  {
+    "revision": "a6dc4656dfc1ff79ee599e853a97b09b",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/code.svg"
+  },
+  {
+    "revision": "29313dcac466c65af70f3559df127238",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/customer-survey.svg"
+  },
+  {
+    "revision": "41c8b67be4a2515b30def619c2535a37",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/design-community.svg"
+  },
+  {
+    "revision": "54046a02fe86b806bed8ade05266d661",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/design-life.svg"
+  },
+  {
+    "revision": "474acf70e8fb4a220e160b38dcbac88f",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/designer.svg"
+  },
+  {
+    "revision": "bc8fcb0fca60814f6c459bab8c2dff0e",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/developer.svg"
+  },
+  {
+    "revision": "ee4d55b8670dd1745aff8f52765ac2ec",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/development.svg"
+  },
+  {
+    "revision": "fbdbcf9ada4f76b0136232c5b133d007",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/email.svg"
+  },
+  {
+    "revision": "b7a728eb5053298b02059338e0ed51ba",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/features.svg"
+  },
+  {
+    "revision": "666d4e19b153fdd9f5ab2bcc87ca9ef4",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/git.svg"
+  },
+  {
+    "revision": "4957c8ca375363c7df56b295b2fa470f",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/group-chat.svg"
+  },
+  {
+    "revision": "0a110c0f0e0d25dae959517bb75717fb",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/hosting.svg"
+  },
+  {
+    "revision": "13831ecc4ebb312957f61cfb4bc2ffd2",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/ipad-report.svg"
+  },
+  {
+    "revision": "002932c1f772d07feae6b1e8736b4ed1",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/ipad.svg"
+  },
+  {
+    "revision": "e737ede8620926299d4c50ea60c88400",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/iphone-hand.svg"
+  },
+  {
+    "revision": "2dfb0a7d44bfc4b4496d824d765fb993",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/mobile-marketing.svg"
+  },
+  {
+    "revision": "9c4a633a2d5b5060e1948fd14ab07438",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/morning-essentials.svg"
+  },
+  {
+    "revision": "d9d021d5509f3e8f3f9ec0dfa1774a52",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/opened.svg"
+  },
+  {
+    "revision": "a566abfaf058a2425e4c54abafd0b394",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/product-tour.svg"
+  },
+  {
+    "revision": "c02d1dfb23645153c58f6e7a8f6411e1",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/rocket.svg"
+  },
+  {
+    "revision": "94798b8c487c2edb773c46d93ab82c98",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/scrum.svg"
+  },
+  {
+    "revision": "7e646bf2421d54728edc5a2dbc4acf3c",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/simple-iphone.svg"
+  },
+  {
+    "revision": "d796c734e66ba65c1a366d0616487dc6",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/smile.svg"
+  },
+  {
+    "revision": "ea127fad697d43902a72f9456344d367",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/social-media.svg"
+  },
+  {
+    "revision": "817c7d67a67b6886889cb8c00730fb70",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/sync.svg"
+  },
+  {
+    "revision": "eadef5d36edbaa8f8ced4117d5bbc08b",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/sync2.svg"
+  },
+  {
+    "revision": "07029178dca20242bfb1f4fddfe0e328",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/tabs.svg"
+  },
+  {
+    "revision": "a7e59b588ff6f49a19b1b7e835ec8587",
+    "url": "/bootstrap-ui-portfolio/imgs/draws/tenis.svg"
+  },
+  {
+    "revision": "d42acd6c704bc0013f320ee82ba6900f",
+    "url": "/bootstrap-ui-portfolio/imgs/hero/blue.svg"
+  },
+  {
+    "revision": "ead9a1a708b859bb805ed5791a796b16",
+    "url": "/bootstrap-ui-portfolio/imgs/hero/purple.svg"
+  },
+  {
+    "revision": "152965c67d5414cad22a8a4a1d7f2857",
+    "url": "/bootstrap-ui-portfolio/imgs/hero/red.svg"
+  },
+  {
+    "revision": "0ff8f2cf756c65f397e5d38c01e938f1",
+    "url": "/bootstrap-ui-portfolio/imgs/hero/yellow.svg"
+  },
+  {
+    "revision": "5eafdae967fc579e0fc5ca79462beab6",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/cloud.svg"
+  },
+  {
+    "revision": "003dced3909174ebea362aa5366d19fc",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/coffee.svg"
+  },
+  {
+    "revision": "c249e0ec7f3c012b462e3e2dcd24c7c5",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/compass.svg"
+  },
+  {
+    "revision": "3fa6c793d67908e8f84e1d2ccfa382db",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/gift.svg"
+  },
+  {
+    "revision": "dd957a21427f9c8773e230113e60686c",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/github.svg"
+  },
+  {
+    "revision": "31a7bb1cccf561860ac6d605deccfff9",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/layers.svg"
+  },
+  {
+    "revision": "ccd55218fb5e96e07776173942c3e501",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/layout.svg"
+  },
+  {
+    "revision": "43eb0c6af16bf58ed6f0b6a42781753b",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/life-buoy.svg"
+  },
+  {
+    "revision": "7c174362f1f7f3558667e186f603b396",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/mail.svg"
+  },
+  {
+    "revision": "898c685ad495968d991e10f48ce8eaf6",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/map-pin.svg"
+  },
+  {
+    "revision": "5c7818b98043a6d233a394766b6c0588",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/map.svg"
+  },
+  {
+    "revision": "349c7f6601940f1faeb3714dc8d27ffb",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/monitor.svg"
+  },
+  {
+    "revision": "908d15a34c6d6d37fddf978de923eb20",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/navigation.svg"
+  },
+  {
+    "revision": "d42fae95fb49b5b28c7fe2cc3b05928a",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/package.svg"
+  },
+  {
+    "revision": "1b44367cc3696246e2a00e8979f54841",
+    "url": "/bootstrap-ui-portfolio/imgs/icons/phone.svg"
+  },
+  {
+    "revision": "4bd50c3f07fad1be55f35c3933ac6801",
+    "url": "/bootstrap-ui-portfolio/imgs/logo.png"
+  },
+  {
+    "revision": "3abc7aef854696388e2c9d271872b037",
+    "url": "/bootstrap-ui-portfolio/imgs/people/1.jpg"
+  },
+  {
+    "revision": "9852f2c10f2da1a016dc931b27e2be50",
+    "url": "/bootstrap-ui-portfolio/imgs/people/2.jpg"
+  },
+  {
+    "revision": "5f5eb1be465c0c025f3a7691d794f7eb",
+    "url": "/bootstrap-ui-portfolio/imgs/people/3.jpg"
+  },
+  {
+    "revision": "1b968d631f4f07e298aaf0091c7c4f10",
+    "url": "/bootstrap-ui-portfolio/imgs/people/4.jpg"
+  },
+  {
+    "revision": "ca3f23dae251f5e2d935edcd6f20eb4d",
+    "url": "/bootstrap-ui-portfolio/imgs/people/5.jpg"
+  },
+  {
+    "revision": "1eb15462b3d47bd4708b5dc7df8bf237",
+    "url": "/bootstrap-ui-portfolio/imgs/people/6.jpg"
+  },
+  {
+    "revision": "adb3455596dbbab9962636dfd22c09cb",
+    "url": "/bootstrap-ui-portfolio/imgs/people/7.jpg"
+  },
+  {
+    "revision": "41ab22c541219361dadabd4511c9e6ad",
+    "url": "/bootstrap-ui-portfolio/imgs/people/8.jpg"
+  },
+  {
+    "revision": "f52ab50caf22000e3fb3a2526f3edf94",
+    "url": "/bootstrap-ui-portfolio/imgs/people/9.jpg"
+  },
+  {
+    "revision": "7efb89faea70f1eb5fff859ad0db278c",
+    "url": "/bootstrap-ui-portfolio/imgs/photos/fireworks-1.jpg"
+  },
+  {
+    "revision": "84d9540c540145a48355612103b04eb1",
+    "url": "/bootstrap-ui-portfolio/imgs/photos/fireworks-2.jpg"
+  },
+  {
+    "revision": "50897894752464d4df627b2ce57aabbd",
+    "url": "/bootstrap-ui-portfolio/imgs/photos/map-1.jpg"
+  },
+  {
+    "revision": "0affd4d2bee4fb9d71134cc9c9292aac",
+    "url": "/bootstrap-ui-portfolio/imgs/photos/map-2.jpg"
+  },
+  {
+    "revision": "9dc7bf0e8260597b63e6b74a9ed4f7f4",
+    "url": "/bootstrap-ui-portfolio/imgs/photos/map-3.jpg"
+  },
+  {
+    "revision": "c420837bae60e94713438ba526b07bf9",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/1.svg"
+  },
+  {
+    "revision": "00632388f03ed7986fa62210c095ec6e",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/10.svg"
+  },
+  {
+    "revision": "f8130effe75ced899b581ef54510daaf",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/2.svg"
+  },
+  {
+    "revision": "fad0d7e551bf30f3db762004b226622f",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/4.svg"
+  },
+  {
+    "revision": "f8e6c8ebfb7c868214e41b7c9c718c43",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/5.svg"
+  },
+  {
+    "revision": "c054b50beaf8506b486d05fbf4c2ba85",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/6.svg"
+  },
+  {
+    "revision": "eab5f201b57baf6b2992e1c73fe60100",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/7.svg"
+  },
+  {
+    "revision": "efa43469c0ca80af3bf07ca5be62ba96",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/8.svg"
+  },
+  {
+    "revision": "251a027492c0b3a18e81dc337a715c9a",
+    "url": "/bootstrap-ui-portfolio/imgs/shapes/9.svg"
+  },
+  {
+    "revision": "cf8bde0c348097090b75b8647a9a0694",
+    "url": "/bootstrap-ui-portfolio/index.html"
+  },
+  {
+    "revision": "2975aa9d757f961b3743",
+    "url": "/bootstrap-ui-portfolio/js/app.20f2da8d.js"
+  },
+  {
+    "revision": "d6c6b41fe9efff104907",
+    "url": "/bootstrap-ui-portfolio/js/chunk-vendors.090fab24.js"
+  },
+  {
+    "revision": "ff27e50ae34cda8c5ebf648e60e0f8f3",
+    "url": "/bootstrap-ui-portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bootstrap-ui-portfolio/robots.txt"
+  }
+]);

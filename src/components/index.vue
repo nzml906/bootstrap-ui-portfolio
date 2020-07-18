cd4cf33ca64173de774b0f127b1d078c32533f05
Nazmul <grey000906@gmail.com>
2020-07-18 06:03:49 +0600

    final build

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <p>index</p>
+    <!-- Contents 1 -->
+    <section class="fdb-block">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col col-md-8 text-center">
+            <h1>
+              Please Click Above Links To View Each Categories
+              Web-UI-Design-Blocks
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
